7cd8dd97cc617eeb28aee71f542f08e223d6e3b3
jonathan.pueyo <jonathan.pueyo@auleseebe.upc.edu>
2025-02-11 09:18:04 +0100

    resolucion problema 1

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,34 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades <- mtcars
+
+mis_dades         # data.frame
+dim(mis_dades)    # numero de individuos y de variables
+
+mis_dades$qsec    # extrae un vector
+
+
+# · medida de centralidad
+# media o promedio [centro de gravedad de los datos] (x^-) (da un valor esperado, un orden de magnitud) 
+mean(mis_dades$qsec)
+
+# mediana [2º cuartil = q0.5] => organiza los datos y corta por el medio
+sort(mis_dades$drat)   #ordena la columna de datos
+median(mis_dades$drat)
+
+# · medida de dispersion
+quantile(mis_dades$mpg, 0.25) # [1er cuartil = q0.25]
+quantile(mis_dades$mpg, 0.75) # [3er cuartil = q0.75]
+IQR(mis_dades$mpg)            # [Inter Quartail Range]
+boxplot(mis_dades$mpg)
+
+
+quantile(mis_dades$mpg, 0.18)
+
+IQR(mis_dades$cyl)
+
+sd(mis_dades$cyl) # desviacion tipica de cyl
+
+var(mis_dades$qsec) # varianza muestral de qsec
+
+
