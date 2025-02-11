1e8b2077b210b25084b831ba29c7af8a3efecaf1
jonathan.pueyo <jonathan.pueyo@auleseebe.upc.edu>
2025-02-11 09:56:02 +0100

    resolucion problemas 2 y 3

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,36 @@
 #Codigo para problema 2
+
+# tabla de frecuencias
+mis_dades$cyl
+
+# frecuencia absoluta
+ni <- table(mis_dades$cyl)
+barplot(ni)
+
+# frecuencia relativa (en porcentaje)
+fi <- ni/length(mis_dades$cyl)
+pie(fi)   # grafico de sectores
+
+# frecuencia acumulada (hasta X valor = coches con hasta 6 cilindros incluye de 4 y 6 cilindros)
+Ni <- cumsum(ni)
+
+# frecuencia relativa acumulada (en porcentaje)
+Fi <- Ni/length(mis_dades$cyl)
+
+# resumen
+cbind(ni,fi,Ni,Fi)
+
+
+
+x <- mis_dades$mpg
+y <- cut(x, 10)
+
+ni <- table(y)
+fi <- ni/length(y)
+Ni <- cumsum(ni)
+Fi <- Ni/length(y)
+cbind(ni,fi,Ni,Fi)
+hist(x, breaks=10) #histograma
+
+# respuesta pregunta 2 => hist(mis_dades$mpg)
+
